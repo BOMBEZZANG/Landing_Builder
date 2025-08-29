436e83ce5029fa176a7b764d4dde1ecbafe66f08
BOMBEZZANG <bombezzang100@gmail.com>
2025-08-29 12:02:51 +0700

    firebase3

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ export function getFirebaseConfig() {
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_measurementId
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID  // 대문자 M과 언더스코어
   };
 }
 
